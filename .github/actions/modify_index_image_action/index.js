9d3ae29a62e62f70dc53357cbc4e6c4c712b281e
dawEstacio <vidal_jossal@iestacio.com>
2021-01-26 10:16:14 +0100

    first commit

---
+++ .github/actions/modify_index_image_action/index.js
@@ -0,0 +1,20 @@
+const core = require("@actions/core");
+const fs = require("fs");
+var filePath = "./index.html";
+
+const randomPosition = Math.floor(Math.random() * 4) + 1;
+const gifs_alternative = core.getInput("gifs_alternative_" + randomPosition);
+
+try {
+  fs.readFile(filePath, "utf8", function (err, data) {
+    var gifUrl = gifs_alternative;
+    var result = data.replace(/src=.* alt/, 'src="' + gifUrl + '" alt');
+    fs.writeFile(filePath, result, "utf8", function (err) {
+      if (err) return console.log(err);
+    });
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+core.setOutput("response", "Imagen modificada");
